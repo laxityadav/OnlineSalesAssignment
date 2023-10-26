ac28f9db366f5958f5f5f2ac5b4563ae29861820
laxit <laxityadav123@gmail.com>
2023-10-26 18:11:48 +0530

    Task1 completed with unit tests

--- src/main/java/org/example/task1/AssignmentTask1.java
+++ src/main/java/org/example/task1/AssignmentTask1.java
@@ -1,2 +1,61 @@
-package org.example.task1;public class AssignmentTask1 {
+package org.example.task1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class AssignmentTask1 {
+    static class Range {
+        int start, end;
+        Range(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Map to represent input data with key-value as numbers and their probabilities
+        Map<Integer, Integer> inputMap = new HashMap<>();
+        inputMap.put(1, 10);
+        inputMap.put(2, 30);
+        inputMap.put(3, 15);
+        inputMap.put(4, 15);
+        inputMap.put(5, 30);
+        inputMap.put(6, 0);
+
+        // Convert the input data into a map of ranges
+        Map<Integer, Range> rangeMap = convertIntoRange(inputMap);
+
+        // Create a map to count the occurrences of each range
+        Map<Integer, Integer> occurrenceMap = new HashMap<>();
+
+        // Simulate 1000 random iterations and count occurrences within the ranges
+        for (int i = 0; i < 1000; i++) {
+            int rNum = (int) (Math.random() * 100) + 1; // Generate a random number between 1 and 100
+            for (Map.Entry<Integer, Range> entry : rangeMap.entrySet()) {
+                Range r = entry.getValue();
+                if (rNum >= r.start && rNum <= r.end) {
+                    // Increment the occurrence count for the corresponding range
+                    occurrenceMap.put(entry.getKey(), 1 + occurrenceMap.getOrDefault(entry.getKey(), 0));
+                }
+            }
+        }
+
+        // Print the occurrence count for each range
+        System.out.println(occurrenceMap);
+    }
+
+    public static Map<Integer, Range> convertIntoRange(Map<Integer, Integer> inputMap) {
+        Map<Integer, Range> rangeMap = new HashMap<>();
+        int min = 0;
+        for (Map.Entry<Integer, Integer> entry : inputMap.entrySet()) {
+            if (entry.getValue() == 0) {
+                continue; // Skip ranges with a value of 0
+            }
+            Range range = new Range(min+1, min + entry.getValue());
+            rangeMap.put(entry.getKey(), range);
+            min += entry.getValue();
+        }
+        return rangeMap;
+    }
 }
--- src/test/java/org/example/task1/AssignmentTask1Test.java
+++ src/test/java/org/example/task1/AssignmentTask1Test.java
@@ -1,4 +1,54 @@
+package org.example.task1;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
-class AssignmentTask1Test {
-  
+
+public class AssignmentTask1Test {
+        @Test
+        public void testConvertIntoRangeForDices() {
+            // Create a test input map
+            Map<Integer, Integer> inputMap = new HashMap<>();
+            inputMap.put(1, 10);
+            inputMap.put(2, 30);
+            inputMap.put(3, 15);
+            inputMap.put(4, 15);
+            inputMap.put(5, 30);
+            inputMap.put(6, 0);
+
+            // Call the conversion method
+            Map<Integer, AssignmentTask1.Range> rangeMap = AssignmentTask1.convertIntoRange(inputMap);
+
+            // Check the size of the resulting range map
+            assertEquals(5, rangeMap.size());
+
+            // Check the values of specific ranges
+            assertTrue(rangeMap.get(1).start==1 && rangeMap.get(1).end==10);
+            assertTrue(rangeMap.get(2).start==11 && rangeMap.get(2).end==40);
+            assertTrue(rangeMap.get(3).start==41 && rangeMap.get(3).end==55);
+            assertTrue(rangeMap.get(4).start==56 && rangeMap.get(4).end==70);
+            assertTrue(rangeMap.get(5).start==71 && rangeMap.get(5).end==100);
+        }
+
+    @Test
+    public void testConvertIntoRangeForCoin() {
+        // Create a test input map
+        Map<Integer, Integer> inputMap = new HashMap<>();
+        inputMap.put(1, 35);
+        inputMap.put(2, 65);
+
+        // Call the conversion method
+        Map<Integer, AssignmentTask1.Range> rangeMap = AssignmentTask1.convertIntoRange(inputMap);
+
+        // Check the size of the resulting range map
+        assertEquals(2, rangeMap.size());
+
+        // Check the values of specific ranges
+        assertTrue(rangeMap.get(1).start==1 && rangeMap.get(1).end==35);
+        assertTrue(rangeMap.get(2).start==36 && rangeMap.get(2).end==100);
+    }
+
 }
